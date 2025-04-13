eb56ff30b90dfc585080e878bbadc49c667a2087
MinhThien <nguyenanminhthien97@gmail.com>
2025-04-13 08:24:59 +0700

    main.py

--- main.py
+++ main.py
@@ -1,148 +1,130 @@
 import datetime
-import subprocess
-import pandas
 import time
+import re
 from selenium.webdriver.common.by import By
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
-from undetected_chromedriver import ChromeOptions
 import undetected_chromedriver as uc
-
-# upwork-scraper.dwg
-# The link to scrape
-# The number of repeated time if use
-# time.sleep(3)
-# The list to store previous value and current value.
-# uusage: python main.py http
-def refresh_page():
+import random
+from undetected_chromedriver import ChromeOptions
+import proxyscrape
+def rotate_proxy():
+    proxy = proxyscrape.proxy
+    return random.choice(proxy)
+def scrape_espn(url):
     global driver
-    driver.refresh()
-    # driver.get(url)
-    try:
-        time.sleep(60)
-        WebDriverWait(driver, 20).until(EC.presence_of_element_located((By.CLASS_NAME, "job-tile-title")))
-    except Exception as e:
-        time.sleep(60)
-        pass
-
-    Header = driver.find_element(By.CLASS_NAME, "job-tile-title")
-    Content = driver.find_element(By.CLASS_NAME, "text-body-sm")
-    Token = driver.find_element(By.CLASS_NAME, "air3-token-container")
-    sub_url = Header.find_element(By.TAG_NAME, 'a').get_attribute('href')
-
-    # Token = driver.find_elements(By.CLASS_NAME, "air3-token-container")
-    # Token_list = [mytoken.text for mytoken in Token]
-    # token_text = ""
-    # for data in Token_list:
-    #     token_text = token_text + data
-    Token_text = ""
-    # Token_text = str.join()
-    for hehe in Token.text.split("\n"):
-        Token_text = Token_text + hehe
-    # mydata = list()
-    # for i in range(Header.__len__()):
-    # mydata.append([Header[i],Content[i],Token[i]])
-    # Only catch the first element that is newest.
-    mydata = [Header.text,Content.text,Token_text, sub_url, datetime.datetime.now().__str__()]
-    return mydata
-
-def check_key_word(text,upwork_key):
-    import re
-    with open(upwork_key,"r") as file:
-        lines = file.readlines()
-        lines = [line.strip() for line in lines if line != ""]
-        true_list = [re.search(line, text) != None for line in lines ]
-        return any(true_list)
-
-
-
-def checking_request_vs_proposal(upwork_key):
+    driver.get(url)
+    time.sleep(2)
+    tomorrow_schedules = driver.find_elements(By.CLASS_NAME,"ResponsiveTable")
+    # heree
+    table = tomorrow_schedules[2].find_element(By.TAG_NAME,"table").find_element(By.TAG_NAME,"tbody")
+    matchups = table.find_elements(By.TAG_NAME,"tr")
+    espn_final = list()
+    espn_finals = list()
+    for idx in range(matchups.__len__()):
+        print(matchups.__len__())
+        # time = .find_element(By.CLASS_NAME,"date__col").text)
+        try:
+            link_game_0 = matchups[idx].find_element(By.CLASS_NAME,"date__col")
+            link_game = link_game_0.find_element(By.TAG_NAME,"a").get_attribute("href")
+            print(link_game,"\n")
+            driver.switch_to.new_window("new_tab")
+            driver.get(link_game)
+            time.sleep(1)
+            print("Getting content in section")
+            espn_news = driver.find_element(By.CLASS_NAME,"PageLayout__Main").find_element(By.XPATH, '//section[@data-testid="prism-LayoutCard"]')
+            # if espn_news[1].text.__contains__("Game Odds"):
+            #     espn_new_element = espn_news[1]
+            # elif espn_news[0].text.__contains__("Game Odds"):
+            #     espn_new_element = espn_news[0]
+            # else:
+            #     raise Exception
+            espn_new = espn_news.text.split("\n")
+            series = [2,7,8,16,17]
+            for serie in series:
+                espn_final.append(re.sub('\(.*\)','',espn_new[serie]))
+            matchup_predictor = driver.find_elements(By.CLASS_NAME, "matchupPredictor__teamValue")
+            print("Finished getting predictor")
+            espn_final.append(matchup_predictor[0].text)
+            espn_final.append(matchup_predictor[1].text)
+            espn_finals.append(espn_final.copy())
+            espn_final.clear()
+            driver.close()
+            driver.switch_to.window(driver.window_handles[0])
+            matchups = table.find_elements(By.TAG_NAME,"tr")
+        except Exception as e:
+            print(f"Exception at: {link_game}\n")
+            pass
+    #     1,2,5 is left; 3,4,6 is right
+    return (espn_finals)
+def scrape_sport_book():
     global driver
-    global cache
-    global count
-    global dataframe
-    # just call above at once, the call the refresh , put these function in a sub_def
-    # when run this script automation, it will raise the web driver everytime, how I can work with the current task ?
-    #         if content exit or not exit, print the notification
-    # os.walk , print the current extracted data into a local file, or store in an list of list, to compare with the later, it there is any not  _> run at remote server, so, no use , where we can find Chromedriver at remote ?
-    #     eachtime refresh page, get the newest element from, just capture the first element of that .
-    mydata = refresh_page()
-    # print(mydata)
-    for sub_content in mydata:
-        # Match our proposal
-        if check_key_word(str(sub_content).lower(),upwork_key):
-            if cache == []:
-                # print(f"{sub_content}")
-                # play_sound.play_sound("note.mp3")
-                # info = input("Do you want to continue ?")
-                # if info == "":
-                #     cache.append(mydata[0])
-                #     continue
-                # else:
-                #     break
-                # cache is used to store all the header name, not the sublist like mydata, cache will not be a single variable because it is used to store for other URLs. cache[0] is for the url[0], cache [1] is for url[1] ,..
-                print(f' \n Attention Attention: \n {datetime.datetime.now()}')
-                print(mydata[0] + "\n" + mydata[1])
-                cache.append(mydata[0])
-                # break to make sure there is no more data is append in the same request
-                break
-            elif cache[0] != mydata[0]:
-                # if sys.argv[1] == "pc":
-                #     play_sound.play_sound("note.mp3")
-                # else:
-                # By default, we run with phone_calls.py
+    driver.switch_to.new_window("newtab")
+    driver.get('https://sportsbook.draftkings.com/leagues/baseball/mlb')
+    time.sleep(2)
+    odd_tables = driver.find_elements(By.CLASS_NAME,"sportsbook-table")
+    if odd_tables.__len__() == 2:
+        print("Script can not run now, output flag to a file outside")
+        odd_table = odd_tables[1]
+    else:
+        odd_table = odd_tables[0]
+    odd_rows = odd_table.find_elements(By.TAG_NAME,"tr")
+    # driver.find_element(By.XPATH,'//table[contain(@class="sportsbook-table")]')
+    draftking_lists = list()
+    draftking_list = list()
 
-                # Alert when the proposal require VietNam people
+    for odd_row in odd_rows:
+        if (odd_row.text.__contains__("TOMORROW") == False):
+            team_name = odd_row.find_element(By.TAG_NAME,"th").text.split("\n")[0]
+            if team_name.__contains__("AM") or team_name.__contains__("PM"):
+                team_name = odd_row.find_element(By.TAG_NAME,"th").text.split("\n")[1]
+            draftking_list.append(team_name)
+            # print(team_name)
+            run_total_money = odd_row.find_elements(By.TAG_NAME,"td")
+            draftking_list.append(run_total_money[0].text)
+            draftking_list.append(run_total_money[1].text)
+            draftking_list.append(run_total_money[2].text)
+            draftking_lists.append(draftking_list.copy())
+            draftking_list.clear()
+    driver.close()
+    driver.switch_to.window(driver.window_handles[0])
+    return (draftking_lists)
 
-                driver.switch_to.new_window('tab')
-                driver.get(mydata[3])
-                sections = driver.find_elements(By.CLASS_NAME, "air3-card-sections")
-                for section in sections:
-                    if "VIETNAM" in section.text.upper():
-                        print(f"URGENT: {mydata[3]}")
-                        mydata[4] = "Urgent"
-                        subprocess.run("python phone_calls.py", shell=True)
-                #  Return back again the old window
-                driver.close()
-                driver.switch_to.window(driver.window_handles[0])
-                pandas.DataFrame(data=mydata).to_csv(path_or_buf="output.csv",mode='a', index=False)
-                play_sound.play_sound("note.mp3")
-                # subprocess.run("python phone_calls.py", shell = True)
+def scrape_rank():
+    global driver
+    driver.switch_to.new_window("newtab")
+    driver.get('https://www.mlb.com/standings/mlb')
+    time.sleep(2)
+    rank_data = (driver.find_element(By.ID, "standings-app-root").text.split("\n"))
+    toggle = 0
+    rank_list = list()
+    for rank_maybe in rank_data:
+        if rank_maybe.__contains__('W L PCT GB'):
+            print("start from here")
+            toggle = 1
+            continue
+        if toggle == 1 and (re.search("\d", rank_maybe)) == None and rank_maybe.strip() != '':
+            rank_list.append(rank_maybe)
+    return rank_list
 
-                #     update the value of cache[0] - the first index point to first match of Request list. Avoid the next time this condition still match
-                print(f' \n Attention Attention: \n {datetime.datetime.now()}')
-                print(mydata[0] + "\n" + mydata[1])
-                cache[0] = mydata[0]
-                break
-            else:
-                count = count + 1
+if __name__ == "__main__":
+    options = ChromeOptions()
+    proxy = rotate_proxy()
+    options.add_argument(f"--proxy-server=http://{proxy}")
+    options.add_argument('--headless=new')
+    driver = uc.Chrome(options)
 
-
-
-if __name__ == '__main__':
-    # the thread running the playsound get the whole process make the time.sleep() can not reach -> work around: use a small mp3 file.
-    # 'https://www.espn.com/mlb/schedule/_/date/20250412'
     url_espn = 'https://www.espn.com/mlb/schedule/_/date/'
-
     current = datetime.datetime.now()
     current_date = current.strftime("%Y") + current.strftime("%m") + current.strftime("%d")
-    url_espn = url_espn + current_date
-    # options = ChromeOptions()
-    # options.add_argument('--incognito')
-    # driver = uc.Chrome(options)
-    driver = uc.Chrome()
-    driver.get(url_espn)
-    try:
-        # Why the schedule package can not work ?
-        # schedule.every(60).seconds.do(checking_request_vs_proposal,url)
-        cache = list()
-        count = 0
-        while (1):
-            time.sleep(10)
-            checking_request_vs_proposal(upwork_key)
-            # Have sleep inside driver.refresh ready
-            # time.sleep(60)
-    except KeyboardInterrupt:
-        driver.quit()
-        pass
+    tomorrow_date =  current.strftime("%Y") + current.strftime("%m") +str(int(current.strftime("%d"))+1)
+    current_espn = url_espn + current_date
+    tomorrow_espn= url_espn + tomorrow_date
+
+    espn_list = scrape_espn(current_espn)
+    print(espn_list)
+    run_total_money_list = scrape_sport_book()
+    print(run_total_money_list)
+    rank_list = scrape_rank()
+    print(rank_list)
+    print("here")
+
 
