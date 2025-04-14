593600e83801577bdabc179c1126f1d7e250c315
MinhThien <nguyenanminhthien97@gmail.com>
2025-04-14 16:05:07 +0700

    Have tested at local new update

--- main_functions.py
+++ main_functions.py
@@ -42,44 +42,72 @@ def scrape_espn(driver,url):
     link_games = list()
     for item in matchups:
         link_game = item.find_element(By.CLASS_NAME, "date__col").find_element(By.TAG_NAME, "a").get_attribute("href")
-        link_games.append(link_game)
+        game_team_pitcher = item.text.split("\n")
+
+        for pitching_matchup in game_team_pitcher:
+            if pitching_matchup.__contains__(" vs "):
+                pitching = pitching_matchup
+                break
+        data_row = [game_team_pitcher[3],game_team_pitcher[0],pitching.split(" vs ")[0],game_team_pitcher[2],pitching.split(" vs ")[1],link_game]
+
+        # data_row: it is combined with all data necessary before enter to each page to get predictor
+        link_games.append(data_row)
 
         # 2.with each linkgame, create the data equivalent to
     espn_final = list()
     espn_finals = list()
     # Create a for loop here to run for each link games have scraped
-    for link_game in link_games:
+    for data_row in link_games:
         try:
-            print(link_game, "\n")
-            driver.get(link_game)
+            print(data_row[-1], "\n")
+            driver.get(data_row[-1])
             WebDriverWait(driver, 5).until(
                 expected_conditions.presence_of_element_located((By.CLASS_NAME, "PageLayout__Main")))
             print("Getting content in section")
-            espn_news = driver.find_element(By.CLASS_NAME, "PageLayout__Main").find_element(By.XPATH,
-                                                                                            '//section[@data-testid="prism-LayoutCard"]')
-            # Surely will get corrected, no use this
-            # if espn_news[1].text.__contains__("Game Odds"):
-            #     espn_new_element = espn_news[1]
-            # elif espn_news[0].text.__contains__("Game Odds"):
-            #     espn_new_element = espn_news[0]
-            # else:
-            #     raise Exception
-            espn_new = espn_news.text.split("\n")
-            series = [2, 7, 8, 16, 17]
-            for serie in series:
-                espn_final.append(re.sub('\(.*\)', '', espn_new[serie]))
+            # espn_news = driver.find_element(By.CLASS_NAME, "PageLayout__Main").find_element(By.XPATH,
+            #                                                                                 '//section[@data-testid="prism-LayoutCard"]')
+            # # Surely will get corrected, no use this
+            # # if espn_news[1].text.__contains__("Game Odds"):
+            # #     espn_new_element = espn_news[1]
+            # # elif espn_news[0].text.__contains__("Game Odds"):
+            # #     espn_new_element = espn_news[0]
+            # # else:
+            # #     raise Exception
+            # espn_new = espn_news.text.split("\n")
+            # series = [2, 7, 8, 16, 17]
+            # for serie in series:
+            #     espn_final.append(re.sub('\(.*\)', '', espn_new[serie]))
             matchup_predictor = driver.find_elements(By.CLASS_NAME, "matchupPredictor__teamValue")
             print("Finished getting predictor")
+            espn_final = data_row[:5]
             espn_final.append(matchup_predictor[0].text)
             espn_final.append(matchup_predictor[1].text)
-            espn_final.append(link_game)
+            espn_final.append(data_row[-1])
             espn_finals.append(espn_final.copy())
             espn_final.clear()
         except Exception as e:
-            print(f"Exception at: {link_game}\n")
+            print(f"Exception at: {data_row}\n")
             pass
     #     1,2,5 is left; 3,4,6 is right
-    return (espn_finals)
+    dict_match_name = dict()
+    for rtm in espn_finals:
+        mismatch_name1 = rtm[1]
+        mismatch_name2 = rtm[3]
+        for item in rank_list:
+            if item.__contains__(mismatch_name1.split()[-1]):
+                dict_match_name[mismatch_name1] = item
+            elif item.__contains__(mismatch_name2.split()[-1]):
+                dict_match_name[mismatch_name2] = item
+        #         Assign again the value of some wrong assign dueto the previous one.
+    dict_match_name['Chicago'] = 'Chicago Cubs'
+    dict_match_name['New York'] = 'New York Yankees'
+    dict_match_name['Los Angeles'] = 'Los Angeles Dodgers'
+    print(dict_match_name)
+    for item in espn_finals:
+        item[1] = dict_match_name[item[1]]
+        item[3] = dict_match_name[item[3]]
+
+    return espn_finals
 
 
 def scrape_sport_book(driver):
@@ -90,30 +118,31 @@ def scrape_sport_book(driver):
         expected_conditions.presence_of_element_located((By.CLASS_NAME, "sportsbook-table")))
 
     odd_tables = driver.find_elements(By.CLASS_NAME, "sportsbook-table")
-    if odd_tables.__len__() == 2:
-        print("Script can not run now, output flag to a file outside")
-        odd_table = odd_tables[1]
-    else:
-        odd_table = odd_tables[0]
-    odd_rows = odd_table.find_elements(By.TAG_NAME, "tr")
+    # if odd_tables.__len__() == 2:
+    #     print("Script can not run now, output flag to a file outside")
+    #     odd_table = odd_tables[1]
+    # else:
+    #     odd_table = odd_tables[0]
 
-    # 2.Scrape rows in that table
+    # Edit for remote server, no matter table is devided
     draftking_lists = list()
     draftking_list = list()
-
-    for odd_row in odd_rows:
-        if (odd_row.text.__contains__("TOMORROW") == False):
-            team_name = odd_row.find_element(By.TAG_NAME, "th").text.split("\n")[0]
-            if team_name.__contains__("AM") or team_name.__contains__("PM"):
-                team_name = odd_row.find_element(By.TAG_NAME, "th").text.split("\n")[1]
-            draftking_list.append(team_name)
-            # print(team_name)
-            run_total_money = odd_row.find_elements(By.TAG_NAME, "td")
-            draftking_list.append(run_total_money[0].text)
-            draftking_list.append(run_total_money[1].text)
-            draftking_list.append(run_total_money[2].text)
-            draftking_lists.append(draftking_list.copy())
-            draftking_list.clear()
+    for odd_table in odd_tables:
+        odd_rows = odd_table.find_elements(By.TAG_NAME, "tr")
+        # 2.Scrape rows in that table
+        for odd_row in odd_rows:
+            if (odd_row.text.__contains__("TOMORROW") == False):
+                team_name = odd_row.find_element(By.TAG_NAME, "th").text.split("\n")[0]
+                if team_name.__contains__("AM") or team_name.__contains__("PM"):
+                    team_name = odd_row.find_element(By.TAG_NAME, "th").text.split("\n")[1]
+                draftking_list.append(team_name)
+                # print(team_name)
+                run_total_money = odd_row.find_elements(By.TAG_NAME, "td")
+                draftking_list.append(run_total_money[0].text)
+                draftking_list.append(run_total_money[1].text)
+                draftking_list.append(run_total_money[2].text)
+                draftking_lists.append(draftking_list.copy())
+                draftking_list.clear()
     return draftking_lists
 
 
@@ -131,7 +160,7 @@ def scrape_rank(driver):
             print("start from here")
             toggle = 1
             continue
-        if toggle == 1 and (re.search("\d", rank_maybe)) == None and rank_maybe.strip() != '':
+        if toggle == 1 and (re.search("\d", rank_maybe)) == None and rank_maybe.strip() != '' and rank_maybe.__contains__('Glossary') == False:
             if not (rank_maybe.__contains__("@") or rank_maybe.__contains__("vs")):
                 rank_list.append(rank_maybe)
     return rank_list
